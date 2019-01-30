65a733e2e8875a9f43b6aed59158322e471bb626
CodeDestroyer2806 <38956102+CodeDestroyer2806@users.noreply.github.com>
2019-01-29 18:01:03 -0600

    Finished Autonomous
    This is basically my full idea for autonomous and WARNING: THE TIME PERIODS MAY BE OFF AND  NOT ACCURATE, I HAVE NOT CALCULATED THE TIME PERIODS CORRECTLY.

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -15,18 +15,18 @@ void autonomous() {
     * 4. Park on alliance deck
     */
 
-    revUpLauncher(); Luke, This is anish and I think you need to lower the speed of the launcher for the AUTONOMOUS PERIOD ONLY so that our robot is able to hit the flags.
-    launcher = 1110 try this motor speed.
+    revUpLauncher(); //Luke, This is anish and I think you need to lower the speed of the launcher for the AUTONOMOUS PERIOD ONLY so that our robot is able to hit the flags.
+    launcher = 1110 //try this motor speed.
 
     // Move forward
-    forward(0.175, 127); * This moving forward command is putting to much momentem on the robot in the far side program (RED FAR OR BLUE FAR).
+    forward(0.175, 127); // This moving forward command is putting to much momentem on the robot in the far side program (RED FAR OR BLUE FAR).
 
     // Face flags
     turnRight(0.32, 127);
 
     // Wait for launcher to rev up
-    pros::delay(5000); Try to shorten the period of delay to like 4.5 seconds or 4 seconds if you plan on doing motor on full speed, this is causing the rubber bands to fall out.
-    pros::delay(4000) Try this command and tell me if it works
+    pros::delay(5000); //Try to shorten the period of delay to like 4.5 seconds or 4 seconds if you plan on doing motor on full speed, this is causing the rubber bands to fall out.
+    pros::delay(4000) //Try this command and tell me if it works
     // Launch
     mtr_combine = -127;
     pros::delay(500);
@@ -36,7 +36,7 @@ void autonomous() {
     forward(1, 127);
 
     // Turn toward platform
-    turnRight(0.3, 127); I think this command would be turn left.
+    turnRight(0.3, 127); //I think this command would be turn left.
     turnLeft(.5,127)
     // Go toward platform
     forward(2, 127);
@@ -50,9 +50,15 @@ void autonomous() {
     // Stop
     zeroAllMotors();
 }
-Another solution would be to try to hit the cone with commands:
+//Another solution would be to try to hit the cone with commands:
 goBack(motorport#,-127) for .2 sec*first motor
 goBack(motorport#,-127) for .2 sec*second motor for wheel
+// this would go after the move forward
 turnLeft(motorport#,127) for .2sec * this sends the right motor straight forward
 turnLeft(motorport#-127) for .2 sec *this ,moves the right motor back and rotates the robot left
  goForward(motorport#,127) for .1 sec * this moves the right motor forward
+ goForward(motorport#,127) for .1 sec * this moves the left motor forward.
+ goForward(motorport#,-127)for 1 sec // this will move the combine so that the cap can be flipped
+ goForward(motorport#,-127) for 1 sec // this will trigger the other combine to do the same as the code segment above.
+ //  This goes after turn toward platform
+ // This is my idea for the new autonomous, tell me if this is good or not. If it is good then we can test it. WARNING: I HAVE NOT MADE THE CORRET CALCULATIONS.   
